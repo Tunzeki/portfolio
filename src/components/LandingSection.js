57f4b782e8cb7b0b2f13757b7fc9684ac1598d52
Tunzeki <tundewebhead@gmail.com>
2023-04-19 21:50:04 +0100

    Add avatar, a greeting and a brief role dsc

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Avatar, Heading, VStack } from "@chakra-ui/react";
 import FullScreenSection from "./FullScreenSection";
 
-const greeting = "Hello, I am Pete!";
+const greeting = "Hello, I am Babatunde!";
 const bio1 = "A frontend developer";
 const bio2 = "specialised in React";
 
@@ -15,6 +15,19 @@ const LandingSection = () => (
     isDarkBackground
     backgroundColor="#2A4365"
   >
+    <VStack>
+      <Avatar url="https://i.pravatar.cc/150?img=7" />
+      <Heading as='h1' size='sm'>
+        {greeting}
+      </Heading>
+      <Heading as='h2' size='xl'>
+        {bio1}
+      </Heading>
+      <Heading as='h2' size='xl'>
+        {bio2}
+      </Heading>
+      
+    </VStack>
 
   </FullScreenSection>
 );
