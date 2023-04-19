7714623d8a7179e629075b3c88c7c10a078ee4f9
Tunzeki <tundewebhead@gmail.com>
2023-04-19 21:28:56 +0100

    Fix bug: missing closing brackets and semicolon

--- src/components/Header.js
+++ src/components/Header.js
@@ -40,64 +40,65 @@ const Header = () => {
       element.scrollIntoView({
         behavior: "smooth",
         block: "start",
-    }
-  };
+      });
+    };
+  }
 
-  return (
-    <Box
-      position="fixed"
-      top={0}
-      left={0}
-      right={0}
-      translateY={0}
-      transitionProperty="transform"
-      transitionDuration=".3s"
-      transitionTimingFunction="ease-in-out"
-      backgroundColor="#18181b"
-    >
-      <Box color="white" maxWidth="1280px" margin="0 auto">
-        <HStack
-          px={16}
-          py={4}
-          justifyContent="space-between"
-          alignItems="center"
-        >
-          <nav>
-            {/* Add social media links based on the `socials` data */}
-            
-            <HStack>
-              {/* Email */}
-              <a href={socials[0].url}>
-                <FontAwesomeIcon icon={socials[0].icon} size="2x" />
-              </a>
-              {/* GitHub */}
-              <a href={socials[1].url}>
-                <FontAwesomeIcon icon={socials[1].icon} size="2x" />
-              </a>
-              {/* LinkedIn */}
-              <a href={socials[2].url}>
-                <FontAwesomeIcon icon={socials[2].icon} size="2x" />
-              </a>
-            </HStack>
+    return (
+      <Box
+        position="fixed"
+        top={0}
+        left={0}
+        right={0}
+        translateY={0}
+        transitionProperty="transform"
+        transitionDuration=".3s"
+        transitionTimingFunction="ease-in-out"
+        backgroundColor="#18181b"
+      >
+        <Box color="white" maxWidth="1280px" margin="0 auto">
+          <HStack
+            px={16}
+            py={4}
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            <nav>
+              {/* Add social media links based on the `socials` data */}            
+              <HStack>
+                {/* Email */}
+                <a href={socials[0].url}>
+                  <FontAwesomeIcon icon={socials[0].icon} size="2x" />
+                </a>
+                {/* GitHub */}
+                <a href={socials[1].url}>
+                  <FontAwesomeIcon icon={socials[1].icon} size="2x" />
+                </a>
+                {/* LinkedIn */}
+                <a href={socials[2].url}>
+                  <FontAwesomeIcon icon={socials[2].icon} size="2x" />
+                </a>
+              </HStack>
             
 
-          </nav>
-          <nav>
-            <HStack spacing={8}>
-              {/* Add links to Projects and Contact me section */}
-              {/* Projects */}
-              <a href="#projects-section" onClick={() => handleClick('projects')}>
-                Projects
-              </a>
-              {/* Contact me */}
-              <a href="#contactme-section" onClick={() => handleClick('contactme')}>
-                Contact Me
-              </a>
-            </HStack>
-          </nav>
-        </HStack>
+            </nav>
+            <nav>
+              <HStack spacing={8}>
+                {/* Add links to Projects and Contact me section */}
+                {/* Projects */}
+                <a href="#projects-section" onClick={() => handleClick('projects')}>
+                  Projects
+                </a>
+                {/* Contact me */}
+                <a href="#contactme-section" onClick={() => handleClick('contactme')}>
+                  Contact Me
+                </a>
+              </HStack>
+            </nav>
+          </HStack>
+        </Box>
       </Box>
-    </Box>
-  );
+    );
 };
+  
 export default Header;
