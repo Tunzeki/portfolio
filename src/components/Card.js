08c366966dabaca042c67b282dd737fbc2066d39
Tunzeki <tundewebhead@gmail.com>
2023-05-09 13:22:07 +0100

    Include links to projects in Card.js
    Add link prop to Card component
    Import Link from chakra-ui

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,9 @@
-import { Heading, HStack, Image, Text, VStack, Box } from "@chakra-ui/react";
+import { Heading, HStack, Image, Text, VStack, Box, Link } from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 
-const Card = ({ title, description, imageSrc }) => {
+const Card = ({ title, description, imageSrc, link }) => {
   // Implement the UI for the Card component according to the instructions.
   // You should be able to implement the component with the elements imported above.
   // Feel free to import other UI components from Chakra UI if you wish to.
@@ -15,9 +15,9 @@ const Card = ({ title, description, imageSrc }) => {
             <Image src={imageSrc} alt="Testing" borderRadius="1%"/>
             <Heading as="h3" size="md" color="black" marginTop="10px" marginLeft="10px">{title}</Heading>
             <Text fontSize="sm" color="gray" marginTop="10px" marginLeft="10px">{description}</Text>
-            <Text fontSize="sm" color="black" margin="10px 0 10px 10px">
+            <Link href={link} fontSize="sm" color="black" margin="10px 0 10px 10px" isExternal>
               See more <FontAwesomeIcon icon={faArrowRight} size="1x" />
-            </Text>
+            </Link>
           </Box>
         </VStack>        
       </HStack> 
