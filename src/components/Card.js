c329a84b4f515d6e65bfc2863079275cdb30f66e
Tunzeki <tundewebhead@gmail.com>
2023-05-30 20:11:13 +0100

    Update Card component
    Remove Chakra UI components

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,6 @@
 import { Heading, HStack, Image, Text, VStack, Box, Link } from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { faArrowRight, faExternalLink } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 
 const Card = ({ title, description, imageSrc, link }) => {
@@ -9,18 +9,16 @@ const Card = ({ title, description, imageSrc, link }) => {
   // Feel free to import other UI components from Chakra UI if you wish to.
   return (
     <>
-      <HStack>
-        <VStack>
-          <Box bg="white" borderRadius="1%">
-            <Image src={imageSrc} alt="Testing" borderRadius="1%"/>
-            <Heading as="h3" size="md" color="black" marginTop="10px" marginLeft="10px">{title}</Heading>
-            <Text fontSize="sm" color="gray" marginTop="10px" marginLeft="10px">{description}</Text>
-            <Link href={link} fontSize="sm" color="black" margin="10px 0 10px 10px" isExternal>
-              See more <FontAwesomeIcon icon={faArrowRight} size="1x" />
-            </Link>
-          </Box>
-        </VStack>        
-      </HStack> 
+      <div>
+        <div style={{ backgroundColor: "white", borderRadius: "1%" }}>
+          <img src={imageSrc} alt="Project" style={{ borderRadius: "1%" }} />
+          <div className="bold" style={{ color: "#000", marginLeft: "10px" }}>{title}</div>
+          <div style={{ color: "gray", marginTop: "10px", marginLeft: "10px" }}>{description}</div>
+          <a href={link} target="_blank" style={{ color: "black", margin: "10px 0 10px 10px" }}>
+            See more <FontAwesomeIcon icon={faExternalLink} size="1x" />
+          </a>
+        </div>
+      </div>
     </>
   );
 };
