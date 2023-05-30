6f55d94391fa53212a8ab3d1173a783b1d55cae2
Tunzeki <tundewebhead@gmail.com>
2023-05-30 22:53:26 +0100

    Add useState hook to Card components
    The underlineLink state variable lets you underline the link returned
    in the JSX onMouseEnter and remove link underline onMouseLeave

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,12 @@
 import { Heading, HStack, Image, Text, VStack, Box, Link } from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight, faExternalLink } from "@fortawesome/free-solid-svg-icons";
-import React from "react";
+import { useState } from "react";
 
 const Card = ({ title, description, imageSrc, link }) => {
-  // Implement the UI for the Card component according to the instructions.
-  // You should be able to implement the component with the elements imported above.
-  // Feel free to import other UI components from Chakra UI if you wish to.
+  const [underlineLink, setUnderlineLink] = useState(false);
+
+  let classStyles = underlineLink ? "text-size-80 text-underline" : "text-size-80";
   return (
     <>
       <div>
@@ -14,7 +14,7 @@ const Card = ({ title, description, imageSrc, link }) => {
           <img src={imageSrc} alt="Project" style={{ borderRadius: "1%" }} />
           <div className="bold" style={{ color: "#000", marginLeft: "10px" }}>{title}</div>
           <div className="text-size-80 ml-10 mr-10" style={{ color: "gray", }}>{description}</div>
-          <a href={link} target="_blank" style={{ color: "black", margin: "10px 0 10px 10px" }}>
+          <a href={link} target="_blank" className={classStyles} onMouseEnter={() => setUnderlineLink(true)} onMouseLeave={() => setUnderlineLink(false)} style={{ color: "black", margin: "10px 0 10px 10px" }}>
             See more <FontAwesomeIcon icon={faExternalLink} size="1x" />
           </a>
         </div>
