044069c2afbb73efc42c54acbb8ba7ec91814933
Tunzeki <tundewebhead@gmail.com>
2023-05-31 17:08:43 +0100

    Make section heading bold

--- src/components/ContactMeSection.js
+++ src/components/ContactMeSection.js
@@ -1,7 +1,7 @@
 const ContactMeSection = () => {
   return (
     <div  className="purple-shade text-white text-center pt-20 pb-20">
-      <h1 className="text-size-140" id="contactme-section">Contact Me</h1>
+      <h1 className="text-size-140 bold" id="contactme-section">Contact Me</h1>
       <p className="text-size-120">
         I have an outstanding work ethic.
       </p>
